fdd0f3b0fb5aff793f7f9f1aa875bc5215c90210
Kauymbaev Kalizhan <mr.kauymbaev@mail.ru>
2021-01-27 11:33:04 +0600

    'recylcerView'

--- app/src/main/java/com/example/firebase/ListTasks.java
+++ app/src/main/java/com/example/firebase/ListTasks.java
@@ -1,58 +1,89 @@
-package com.example.firebase;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.GenericTypeIndicator;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.List;
-
-public class ListTasks extends AppCompatActivity {
-
-    private FirebaseAuth mAuth;
-    private DatabaseReference myRef;
-    private List<String> DiscrTasks;
-
-    ListView ListUserTasks;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_tasks);
-
-        ListUserTasks = findViewById(R.id.discr_for_task);
-
-        myRef = FirebaseDatabase.getInstance().getReference();
-
-        FirebaseUser user = mAuth.getInstance().getCurrentUser();
-
-        myRef.child(user.getUid()).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                GenericTypeIndicator<List<String>> t = new GenericTypeIndicator<List<String>>() {};
-                DiscrTasks = dataSnapshot.child("Tasks").getValue(t);
-
-                updateUI();
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-
-            }
-        });
-    }
-    private void updateUI(){
-        ArrayAdapter <String> adapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_list_item_1, DiscrTasks);
-        ListUserTasks.setAdapter(adapter);
-    }
-}
+//package com.example.firebase;
+//
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.ArrayAdapter;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.ListView;
+//import android.widget.TextView;
+//
+//import com.firebase.ui.database.FirebaseListAdapter;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseUser;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.GenericTypeIndicator;
+//import com.google.firebase.database.ValueEventListener;
+//
+//import java.util.List;
+//
+//public class ListTasks extends AppCompatActivity {
+//
+//    private FirebaseAuth mAuth;
+//    private DatabaseReference myRef;
+//
+//    FirebaseUser user = mAuth.getInstance().getCurrentUser();
+//
+//    FirebaseListAdapter mAdapter;
+//
+//    EditText et_new_tasks;
+//    Button btn_add;
+//
+//    ListView ListUserTasks;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_list_tasks);
+//
+//        ListUserTasks = findViewById(R.id.discr_for_task);
+//
+//        myRef = FirebaseDatabase.getInstance().getReference();
+//
+//        mAdapter = new FirebaseListAdapter<String>(this, String.class, android.R.layout.simple_list_item_1, myRef.child(user.getUid()).child("Tasks")) {
+//            @Override
+//            protected void populateView(View v, String s, int position) {
+//                TextView text = v.findViewById(android.R.id.text1);
+//                text.setText(s);
+//            }
+//        };
+//        ListUserTasks.setAdapter(mAdapter);
+//
+//        btn_add = findViewById(R.id.btn_add);
+//        et_new_tasks = findViewById(R.id.et_new_tasks);
+//
+//        btn_add.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                myRef.child(user.getUid()).child("Tasks").push().setValue(et_new_tasks.getText().toString());
+//            }
+//        });
+//
+////        FirebaseUser user = mAuth.getInstance().getCurrentUser();
+////
+////        myRef.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+////            @Override
+////            public void onDataChange(DataSnapshot dataSnapshot) {
+////                GenericTypeIndicator<List<String>> t = new GenericTypeIndicator<List<String>>() {};
+////                DiscrTasks = dataSnapshot.child("Tasks").getValue(t);
+////
+////                updateUI();
+////            }
+////
+////            @Override
+////            public void onCancelled(DatabaseError error) {
+////
+////            }
+////        });
+//    }
+////    private void updateUI(){
+////        ArrayAdapter <String> adapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_list_item_1, DiscrTasks);
+////        ListUserTasks.setAdapter(adapter);
+////    }
+//}
--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -2,93 +2,207 @@ package com.example.firebase;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+import java.util.HashMap;
+import java.util.Map;
 
-    private EditText ETemail, ETpassword;
+public class MainActivity extends AppCompatActivity{
+
+    private EditText editText, etd;
+    private Button button;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private FirebaseRecyclerAdapter adapter;
+//    private FirebaseAuth mAuth;
+//    private FirebaseAuth.AuthStateListener mAuthListener;
+//
+//    private EditText ETemail, ETpassword;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
 
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
+        editText = findViewById(R.id.et);
+        etd = findViewById(R.id.etd);
+        button = findViewById(R.id.btn);
+        recyclerView = findViewById(R.id.list);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null){
-                    Intent intent = new Intent(MainActivity.this, ListTasks.class);
-                    startActivity(intent);
-                }else{
+            public void onClick(View v) {
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Tasks").push();
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", databaseReference.getKey());
+                map.put("title", editText.getText().toString());
+                map.put("desc", etd.getText().toString());
 
-                }
+                databaseReference.setValue(map);
+            }
+        });
+
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        fetch();
+
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        mAuthListener = new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                FirebaseUser user = firebaseAuth.getCurrentUser();
+//                if (user != null){
+//                    Intent intent = new Intent(MainActivity.this, ListTasks.class);
+//                    startActivity(intent);
+//                }else{
+//
+//                }
+//            }
+//        };
+//        ETemail = findViewById(R.id.et_email);
+//        ETpassword = findViewById(R.id.et_password);
+//
+//        findViewById(R.id.btn_sign_in).setOnClickListener(this);
+//        findViewById(R.id.btn_registration).setOnClickListener(this);
+//
+//
+//        FirebaseUser user = mAuth.getCurrentUser();
+//        if (user != null) {
+//            Intent intent = new Intent(MainActivity.this, ListTasks.class);
+//            startActivity(intent);
+//        }
+    }
+
+    private void fetch() {
+        Query query = FirebaseDatabase.getInstance().getReference().child("Tasks");
+
+        FirebaseRecyclerOptions<Model> options = new FirebaseRecyclerOptions.Builder<Model>().setQuery(query, new SnapshotParser<Model>() {
+            @NonNull
+            @Override
+            public Model parseSnapshot(@NonNull DataSnapshot snapshot) {
+                return new Model(snapshot.child("id").getValue().toString(),
+                        snapshot.child("title").getValue().toString(),
+                        snapshot.child("desc").getValue().toString());
+            }
+        }).build();
+
+        adapter = new FirebaseRecyclerAdapter<Model, ViewHolder>(options) {
+
+            @NonNull
+            @Override
+            public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+
+                return new ViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Model model) {
+                holder .setTxtTitle(model.getmTitle());
+                holder .setTxtDesc(model.getmDesc());
+
+                holder.root.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, String.valueOf(position), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         };
-        ETemail = findViewById(R.id.et_email);
-        ETpassword = findViewById(R.id.et_password);
+        recyclerView.setAdapter(adapter);
+    }
 
-        findViewById(R.id.btn_sign_in).setOnClickListener(this);
-        findViewById(R.id.btn_registration).setOnClickListener(this);
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public LinearLayout root;
+        public TextView txtTitle, txtDesc;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            root = itemView.findViewById(R.id.list_root);
+            txtTitle = itemView.findViewById(R.id.list_title);
+            txtDesc = itemView.findViewById(R.id.list_desc);
+        }
+
+        public void setTxtTitle(String string) {
+            txtTitle.setText(string);
+        }
 
 
-        FirebaseUser user = mAuth.getCurrentUser();
-        if (user != null) {
-            Intent intent = new Intent(MainActivity.this, ListTasks.class);
-            startActivity(intent);
+        public void setTxtDesc(String string) {
+            txtDesc.setText(string);
         }
     }
 
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btn_sign_in){
-            signing(ETemail.getText().toString(), ETpassword.getText().toString());
-        }else if (v.getId() == R.id.btn_registration){
-            registration(ETemail.getText().toString(), ETpassword.getText().toString());
-        }
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
     }
 
-    public void signing(String email, String password){
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "Sign in is complete!", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(MainActivity.this, "Sign in is not complete!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 
-    public void registration(String email, String password){
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "Registration is complete!", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(MainActivity.this, "Registration is not complete!", Toast.LENGTH_SHORT).show();
-                        }
-
-                    }
-                });
-    }
+//    @Override
+//    public void onClick(View v) {
+//        if (v.getId() == R.id.btn_sign_in){
+//            signing(ETemail.getText().toString(), ETpassword.getText().toString());
+//        }else if (v.getId() == R.id.btn_registration){
+//            registration(ETemail.getText().toString(), ETpassword.getText().toString());
+//        }
+//    }
+//
+//    public void signing(String email, String password){
+//        mAuth.signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            Toast.makeText(MainActivity.this, "Sign in is complete!", Toast.LENGTH_SHORT).show();
+//                        } else {
+//                            Toast.makeText(MainActivity.this, "Sign in is not complete!", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+//    }
+//
+//    public void registration(String email, String password){
+//        mAuth.createUserWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            Toast.makeText(MainActivity.this, "Registration is complete!", Toast.LENGTH_SHORT).show();
+//                        } else {
+//                            Toast.makeText(MainActivity.this, "Registration is not complete!", Toast.LENGTH_SHORT).show();
+//                        }
+//
+//                    }
+//                });
+//    }
 }
